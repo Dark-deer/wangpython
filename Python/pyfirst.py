2490b88135e7f9c85b7e523a7d980e50b26e1953
e <love-asuka@outlook.com>
2017-08-28 18:04:51 +0800

    测试提交

---
+++ Python/pyfirst.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+print('================Python import mode==========================');
+# print ('命令行参数为:')
+# for i in sys.argv:
+#     print (i)
+# print ('\n python 路径为',sys.path)
+#
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(s4)
